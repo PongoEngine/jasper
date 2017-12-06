fb7654afbb4c45dd25b922f3ba95d1368cf152f9
Jeremy <cream@sourcre.am>
2017-12-05 22:55:48 -0800

    add point

--- Sources/jasper/Point.hx
+++ Sources/jasper/Point.hx
@@ -6,7 +6,79 @@
 
 package jasper;
 
+import jasper.variable.Variable;
+import jasper.variable.AbstractVariableArgs;
+
+//Looks complete but not tested
+
 class Point
 {
+    public var x (default, null) :Variable;
+    public var y (default, null) :Variable;
 
+    /**
+     *  [Description]
+     *  @param xVar - 
+     *  @param yVar - 
+     */
+    public function new(xVar :Variable, yVar :Variable) : Void
+    {
+        this.x = xVar;
+        this.y = yVar;
+    }
+
+    /**
+     *  [Description]
+     *  @param xVar - 
+     */
+    public function setX(xVar :Variable) : Void
+    {
+        this.x = xVar;
+    }
+
+    /**
+     *  [Description]
+     *  @param yVar - 
+     */
+    public function setY(yVar :Variable) : Void
+    {
+        this.y = yVar;
+    }
+
+    /**
+     *  [Description]
+     *  @param val - 
+     */
+    public function setXValue(val :Int) : Void
+    {
+        this.x.value = INT(val);
+    }
+
+    /**
+     *  [Description]
+     *  @param val - 
+     */
+    public function setYValue(val :Int) : Void
+    {
+        this.y.value = INT(val);
+    }
+
+    /**
+     *  [Description]
+     *  @param x - 
+     *  @param y - 
+     *  @param suffix - 
+     *  @return Point
+     */
+    public static function fromVals(x :Int, y :Int, ?suffix :String) : Point
+    {
+        var xArgs :AbstractVariableArgs = {value :INT(x)};
+        var yArgs :AbstractVariableArgs = {value :INT(y)};
+        if(suffix != null) {
+            xArgs.name = "x" + suffix;
+            yArgs.name = "y" + suffix;
+        }
+
+        return new Point(new Variable(xArgs), new Variable(yArgs));
+    }
 }
