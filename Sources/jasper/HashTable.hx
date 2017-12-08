d407ece42b600e9da1c942c036587a48138e3c24
Jeremy <cream@sourcre.am>
2017-12-07 21:31:51 -0800

    more of hashtable

--- Sources/jasper/HashTable.hx
+++ Sources/jasper/HashTable.hx
@@ -10,9 +10,13 @@ import jasper.Hashable;
 
 class HashTable<K:Hashable, V> implements Hashable
 {
+    public var hashCode (default, null) :Int;
+    public var size (get, null) :Int;
 
-    public var hashCode (default, null):Int;
-
+    /**
+     *  [Description]
+     *  @param ht - 
+     */
     public function new(?ht :HashTable<K,V>) : Void
     {
         _store = new Map<Int,V>();
@@ -24,14 +28,111 @@ class HashTable<K:Hashable, V> implements Hashable
         }
     }
 
+    /**
+     *  [Description]
+     *  @param key - 
+     *  @return V
+     */
     public function get(key :K) : V
     {
         return _store.get(key.hashCode);
     }
 
+    /**
+     *  [Description]
+     *  @return HashTable<K,V>
+     */
     public function clone() : HashTable<K,V>
     {
         return new HashTable(this);
+    }
+
+    /**
+     *  [Description]
+     */
+    public function clear() : Void
+    {
+        // this._store.clear();
+    }
+
+    /**
+     *  [Description]
+     *  @return Int
+     */
+    private function get_size() :Int
+    {
+    //   return this._store.size;
+        return 0;
+    }
+
+    /**
+     *  [Description]
+     *  @param key - 
+     *  @param value - 
+     */
+    public function set(key :K, value :V) : Void
+    {
+        // if (!key.hashCode) debugger;
+        return this._store.set(key.hashCode, value);
+    }
+
+    /**
+     *  [Description]
+     *  @param key - 
+     *  @return Bool
+     */
+    public function has(key :K) : Bool
+    {
+        return this._store.exists(key.hashCode);
+    }
+
+    /**
+     *  [Description]
+     *  @param key - 
+     */
+    public function delete(key :K) : Void
+    {
+        this._store.remove(key.hashCode);
+    }
+
+    /**
+     *  [Description]
+     *  @param callback - 
+     */
+    public function each(callback : V -> Void) : Void
+    {
+    }
+
+    /**
+     *  [Description]
+     *  @param callback - 
+     */
+    public function escapingEach(callback : V -> Void) : Void
+    {
+    }
+
+    /**
+     *  [Description]
+     *  @param other - 
+     *  @return Bool
+     */
+    public function equals(other :HashTable<K,V>) : Bool
+    {
+        if (other == this) {
+            return true;
+        }
+
+        if (other.size != this.size) {
+            return false;
+        }
+
+        for(x in this._store.keys()) {
+            if (other._store.get(x) == null) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private var _store :Map<Int,V>;
