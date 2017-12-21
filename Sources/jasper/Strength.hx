67d86949970426450e26d0a4511869e3c9e9f72a
Jeremy <cream@sourcre.am>
2017-12-21 13:02:32 -0800

    update licensing info to match nucleic

--- Sources/jasper/Strength.hx
+++ Sources/jasper/Strength.hx
@@ -1,29 +1,14 @@
 /*
+ * Copyright (c) 2013-2017, Nucleic Development Team.
  * Haxe Port Copyright (c) 2017 Jeremy Meltingtallow
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
- * Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
- * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
- * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Distributed under the terms of the Modified BSD License.
+ *  
+ *  The full license is in the file COPYING.txt, distributed with this software.
 */
 
 package jasper;
 
-/**
- * Created by alex on 30/01/15.
- */
 @:notNull
 abstract Strength(Float) to Float
 {
