6fd0d33c885598e586e838eb3289554ba5b9d885
Jeremy <cream@sourcre.am>
2017-12-29 15:26:14 -0800

    lic info headers

--- Sources/jasper/ds/JasperVector.hx
+++ Sources/jasper/ds/JasperVector.hx
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2013-2017, Nucleic Development Team.
+ * Haxe Port Copyright (c) 2017 Jeremy Meltingtallow
+ *
+ * Distributed under the terms of the Modified BSD License.
+ *  
+ * The full license is in the file COPYING.txt, distributed with this software.
+*/
+
 package jasper.ds;
 
 import haxe.ds.Vector;
