1379137795628c3bae7f7e69857e8990c0a4066b
Jeremy <cream@sourcre.am>
2018-01-06 15:14:36 -0800

    begin adding removal

--- Sources/test/Main.hx
+++ Sources/test/Main.hx
@@ -6,33 +6,33 @@ class Main
 {
     public static function main() : Void
     {
-        Tests.simpleNew();
-        Tests.simple0();
-        Tests.simple1();
-        Tests.casso1();
-        // Tests.inconsistent1();
-        // Tests.inconsistent2();
-        // Tests.inconsistent3();
-        Tests.lessThanEqualTo_ConstantVariableTest();
-        // Tests.lessThanEqualToUnsatisfiable_ConstantVariableTest();
-        Tests.greaterThanEqualTo_ConstantVariableTest();
-        // Tests.greaterThanEqualToUnsatisfiable_ConstantVariableTest();
-        Tests.lessThanEqualTo_ExpressionVariableTest();
-        // Tests.lessThanEqualToUnsatisfiable_ExpressionVariableTest();
-        Tests.greaterThanEqualTo_ExpressionVariableTest();
-        // Tests.greaterThanEqualToUnsatisfiable_ExpressionVariableTest();
-        Tests.lessThanEqualTo_VariableConstantTest();
-        // Tests.lessThanEqualToUnsatisfiable_VariableConstantTest();
-        Tests.greaterThanEqualTo_VariableConstantTest();
-        // Tests.greaterThanEqualToUnsatisfiable_VariableConstantTest();
-        Tests.lessThanEqualTo_VariableExpression();
-        // Tests.lessThanEqualToUnsatisfiable_VariableExpression();
-        Tests.greaterThanEqualTo_VariableExpression();
-        // Tests.greaterThanEqualToUnsatisfiable_VariableExpression();
-        Tests.lessThanEqualTo_VariableVariableTest();
-        // Tests.lessThanEqualToUnsatisfiable_VariableVariableTest();
-        Tests.greaterThanEqualTo_VariableVariableTest();
-        // Tests.greaterThanEqualToUnsatisfiable_VariableVariableTest();
+        // Tests.simpleNew();
+        // Tests.simple0();
+        // Tests.simple1();
+        // Tests.casso1();
+        // // Tests.inconsistent1();
+        // // Tests.inconsistent2();
+        // // Tests.inconsistent3();
+        // Tests.lessThanEqualTo_ConstantVariableTest();
+        // // Tests.lessThanEqualToUnsatisfiable_ConstantVariableTest();
+        // Tests.greaterThanEqualTo_ConstantVariableTest();
+        // // Tests.greaterThanEqualToUnsatisfiable_ConstantVariableTest();
+        // Tests.lessThanEqualTo_ExpressionVariableTest();
+        // // Tests.lessThanEqualToUnsatisfiable_ExpressionVariableTest();
+        // Tests.greaterThanEqualTo_ExpressionVariableTest();
+        // // Tests.greaterThanEqualToUnsatisfiable_ExpressionVariableTest();
+        // Tests.lessThanEqualTo_VariableConstantTest();
+        // // Tests.lessThanEqualToUnsatisfiable_VariableConstantTest();
+        // Tests.greaterThanEqualTo_VariableConstantTest();
+        // // Tests.greaterThanEqualToUnsatisfiable_VariableConstantTest();
+        // Tests.lessThanEqualTo_VariableExpression();
+        // // Tests.lessThanEqualToUnsatisfiable_VariableExpression();
+        // Tests.greaterThanEqualTo_VariableExpression();
+        // // Tests.greaterThanEqualToUnsatisfiable_VariableExpression();
+        // Tests.lessThanEqualTo_VariableVariableTest();
+        // // Tests.lessThanEqualToUnsatisfiable_VariableVariableTest();
+        // Tests.greaterThanEqualTo_VariableVariableTest();
+        // // Tests.greaterThanEqualToUnsatisfiable_VariableVariableTest();
 
         Tests.addRemoveConstraint();
     }
