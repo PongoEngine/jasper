f16981023aa80ba6a0c1f68a00f14b1848b0a910
Jeremy <cream@sourcre.am>
2018-01-07 21:58:41 -0800

    adding tests

---
+++ Sources/tests/Main.hx
@@ -0,0 +1,19 @@
+package tests;
+
+import haxe.unit.TestCase;
+
+class Main
+{
+    public static function main() : Void
+    {
+        new Test().run();
+    }
+}
+
+class Test extends TestCase
+{
+    public function run() : Void
+    {
+        assertEquals("a", "b");
+    }
+}
